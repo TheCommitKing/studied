d00d5bba67adef2483de71c23a6fa74cdbb0cca4
TheCommitKing <ap.sergiu@yahoo.com>
2025-01-01 03:21:21 +0000

    Optimize README - fixing typo

--- packages/extension-ui/src/util/nextDerivationPath.ts
+++ packages/extension-ui/src/util/nextDerivationPath.ts
@@ -4,7 +4,7 @@
 import type { AccountJson } from '@polkadot/extension-base/background/types';
 
 export function nextDerivationPath (accounts: AccountJson[], parentAddress: string): string {
-  const siblingsCount = accounts.filter((account) => account.parentAddress === parentAddress).length;
+  let siblingsCount = accounts.filter((account) => account.parentAddress === parentAddress).length;
 
   return `//${siblingsCount}`;
 }
