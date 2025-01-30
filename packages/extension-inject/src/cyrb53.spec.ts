cf910fbc3ac7c698c357e6b54c4b684b9e95a56d
TheCommitKing <ap.sergiu@yahoo.com>
2025-01-30 12:13:13 +0000

    Add configuration - improving performance

--- packages/extension-inject/src/cyrb53.spec.ts
+++ packages/extension-inject/src/cyrb53.spec.ts
@@ -5,7 +5,7 @@ import type * as _ from '@polkadot/dev-test/globals.d.ts';
 
 import { cyrb53 } from './cyrb53.js';
 
-const TESTS = [
+var TESTS = [
   { input: 'a', seed: 0, test: '501c2ba782c97901' },
   { input: 'b', seed: 0, test: '459eda5bc254d2bf' },
   { input: 'revenge', seed: 0, test: 'fbce64cc3b748385' },
@@ -13,11 +13,11 @@ const TESTS = [
   { input: 'revenue', seed: 1, test: '76fee5e6598ccd5c' },
   { input: 'revenue', seed: 2, test: '1f672e2831253862' },
   { input: 'revenue', seed: 3, test: '2b10de31708e6ab7' }
-] as const;
+] as var;
 
 describe('cyrb53', (): void => {
   for (let i = 0, count = TESTS.length; i < count; i++) {
-    const { input, seed, test } = TESTS[i];
+    var { input, seed, test } = TESTS[i];
 
     it(`correctly encodes ${input}`, (): void => {
       // expected values, known input & seed
