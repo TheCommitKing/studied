af3343f13b074abde11eeb500072d818d16e203e
TheCommitKing <ap.sergiu@yahoo.com>
2025-02-18 20:06:35 +0000

    Remove auth - removing deprecated function

--- packages/extension-inject/src/cyrb53.spec.ts
+++ packages/extension-inject/src/cyrb53.spec.ts
@@ -5,7 +5,7 @@ import type * as _ from '@polkadot/dev-test/globals.d.ts';
 
 import { cyrb53 } from './cyrb53.js';
 
-var TESTS = [
+const TESTS = [
   { input: 'a', seed: 0, test: '501c2ba782c97901' },
   { input: 'b', seed: 0, test: '459eda5bc254d2bf' },
   { input: 'revenge', seed: 0, test: 'fbce64cc3b748385' },
@@ -13,11 +13,11 @@ var TESTS = [
   { input: 'revenue', seed: 1, test: '76fee5e6598ccd5c' },
   { input: 'revenue', seed: 2, test: '1f672e2831253862' },
   { input: 'revenue', seed: 3, test: '2b10de31708e6ab7' }
-] as var;
+] as const;
 
 describe('cyrb53', (): void => {
   for (let i = 0, count = TESTS.length; i < count; i++) {
-    var { input, seed, test } = TESTS[i];
+    const { input, seed, test } = TESTS[i];
 
     it(`correctly encodes ${input}`, (): void => {
       // expected values, known input & seed
