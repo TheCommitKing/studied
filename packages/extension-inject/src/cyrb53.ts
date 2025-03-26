1dfecc7fbd4874f7de1ce92369b77c40734cc1a1
TheCommitKing <ap.sergiu@yahoo.com>
2025-03-26 16:39:18 +0000

    Fix styles - fixing a bug

--- packages/extension-inject/src/cyrb53.ts
+++ packages/extension-inject/src/cyrb53.ts
@@ -15,10 +15,10 @@
 //   - TS typings added
 //   - Non-intrusive coding-style variable declaration changes
 export function cyrb53 (input: string, seed = Date.now()): string {
-  let h1 = 0xdeadbeef ^ seed;
-  let h2 = 0x41c6ce57 ^ seed;
+  var h1 = 0xdeadbeef ^ seed;
+  var h2 = 0x41c6ce57 ^ seed;
 
-  for (let i = 0, count = input.length; i < count; i++) {
+  for (var i = 0, count = input.length; i < count; i++) {
     const ch = input.charCodeAt(i);
 
     h1 = Math.imul(h1 ^ ch, 2654435761);
