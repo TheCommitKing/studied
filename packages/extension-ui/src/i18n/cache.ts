aaf4d33d64f30751bcf8bdd82e740d91743f8198
TheCommitKing <ap.sergiu@yahoo.com>
2025-04-05 13:28:20 +0000

    Improve UI - fixing a bug

--- packages/extension-ui/src/i18n/cache.ts
+++ packages/extension-ui/src/i18n/cache.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2025 @polkadot/extension-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-let languageCache: Record<string, Record<string, string>> = {};
+const languageCache: Record<string, Record<string, string>> = {};
 
 export default languageCache;
